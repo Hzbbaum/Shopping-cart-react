e9c14ef69409235249a4dc7759aeda77d84d8c18
Hillel Buchsbaum <Hzbbaum@gmail.com>
2023-01-05 14:50:09 +0200

    can now add to different catagories

--- src/smart components/home/components/addItem.js
+++ src/smart components/home/components/addItem.js
@@ -6,23 +6,40 @@ import CategoriesList from "./categoriesList";
 
 export default function AddItem() {
   const dispatch = useDispatch();
-  const [input, setInput] = useState("");
+  const [itemName, setItemName] = useState("");
+  const [chosenCategory, setchosenCategory] = useState(null);
   // function addCategory() {
   //   dispatch(addCategoryToStore(input));
   // }
-  function addItemToCategory(){
-    dispatch(addItemToCategoryInStore({categoryName: "milk", itemName:input}))
+  function addItemToCategory() {
+    if (chosenCategory !== null && itemName !== "") {
+      dispatch(
+        addItemToCategoryInStore({
+          categoryName: chosenCategory,
+          itemName: itemName,
+        })
+      );
+      setItemName("");
+      setchosenCategory(null);
+    }
   }
 
   function handleChange(e) {
-    setInput(e.target.value);
+    setItemName(e.target.value);
   }
+
   return (
     <div>
-      <input value={input} onChange={handleChange}></input>
+      <div>current category is: {chosenCategory}</div>
+      <input value={itemName} onChange={handleChange}></input>
       {/* <button onClick={addCategory}>submit</button> */}
-      <button onClick={addItemToCategory}>submit</button>
-      <CategoriesList />
+      <button
+        onClick={addItemToCategory}
+        disabled={chosenCategory === null || itemName === ""}
+      >
+        submit
+      </button>
+      <CategoriesList changeCategoryHandler={setchosenCategory} />
     </div>
   );
 }
--- src/smart components/home/components/categoriesList.js
+++ src/smart components/home/components/categoriesList.js
@@ -2,12 +2,20 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectList } from "../../listSlice";
 
-export default function CategoriesList() {
+export default function CategoriesList(props) {
   const list = useSelector(selectList);
   const categoryButtons = list.map((category) => (
-    <button key={category.name} items={category.items} value={category.name}>
+    <button
+      key={category.name}
+      items={category.items}
+      value={category.name}
+      onClick={updateCategory}
+    >
       {category.name}
     </button>
   ));
-  return <div>{categoryButtons}</div>;
+  function updateCategory(e) {
+    props.changeCategoryHandler(e.target.value);
+  }
+  return <React.Fragment>{categoryButtons}</React.Fragment>;
 }
