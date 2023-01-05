d94a12009bfe6ec1f11c7dfed7c3c36d6d314651
Hillel Buchsbaum <Hzbbaum@gmail.com>
2023-01-05 08:51:57 +0200

    added header

---
+++ src/plain components/header/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <header className='bg-primary text-secondary pt-2 pb-2'>
+      <h1 className='ps-4'>Plain Shopping Cart</h1>
+    </header>
+  );
+}
